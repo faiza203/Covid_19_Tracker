73c16025a21db0fd2901dc911819ff88aef47146
faiza203 <fiazfaiza981@gmail.com>
2020-10-28 16:54:16 +0500

    Return response in fecthData function

--- src/api/index.js
+++ src/api/index.js
@@ -1,10 +1,10 @@
 import axios from "axios";
 const url = "https://covid19.mathdro.id/api";
 export const fetchData = async () => {
- try{
-      const response = await axios.get(url);
-    console.log(response);
-}catch(err){
-   console.log(err);
-}
+    try {
+        const response = await axios.get(url);
+        return response;
+    } catch (err) {
+        console.log(err);
+    }
 }
