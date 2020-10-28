bae472da8a3a9d0c20679c40511c5614999793cd
faiza203 <fiazfaiza981@gmail.com>
2020-10-28 16:51:55 +0500

    Insert try and catch in fetchData function

--- src/api/index.js
+++ src/api/index.js
@@ -1,5 +1,10 @@
 import axios from "axios";
 const url = "https://covid19.mathdro.id/api";
 export const fetchData = async () => {
-
+ try{
+      const response = await axios.get(url);
+    console.log(response);
+}catch(err){
+   console.log(err);
+}
 }
