058b71b5b65145e763f1841d2889365c4337a77a
faiza203 <fiazfaiza981@gmail.com>
2020-10-29 12:22:55 +0500

    Add lastUpdate in confirmed casses

--- src/components/Cards/Cards.jsx
+++ src/components/Cards/Cards.jsx
@@ -11,7 +11,7 @@ const Cards = ({data : {confirmed ,recovered , deaths , lastUpdate}}) => {
     <CardContent>
     <Typography color="textSecondary" gutterBottom>Infected</Typography>
     <Typography variant="h5"><CountUp start={0} end ={confirmed.value} duration={2.5} separator=","></CountUp></Typography>
-    <Typography color="textSecondary" >Real Data</Typography>
+    <Typography color="textSecondary" >{lastUpdate}</Typography>
     <Typography variant="body2">Number of active cases of COVID-19</Typography>
     </CardContent></Grid>
     <Grid item component={Card}>
