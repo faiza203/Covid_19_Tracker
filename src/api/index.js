3c2a89809109a021789bb698d412ea6ed0154e41
faiza203 <fiazfaiza981@gmail.com>
2020-10-29 11:26:21 +0500

    Insert modifiedData object

--- src/api/index.js
+++ src/api/index.js
@@ -2,8 +2,14 @@ import axios from "axios";
 const url = "https://covid19.mathdro.id/api";
 export const fetchData = async () => {
     try {
-        const {data} = await axios.get(url);
-        return JSON.stringify(data);
+        const { data: {confirmed, recovered, deaths, lastUpdate} } = await axios.get(url);
+        const modifiedData = {
+            confirmed,
+            recovered,
+            deaths,
+            lastUpdate
+        }
+        return (modifiedData);
     } catch (err) {
         console.log(err);
     }
