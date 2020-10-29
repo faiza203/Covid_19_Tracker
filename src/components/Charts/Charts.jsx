f1a2b6d182f7f5776b143cdacf68161765d6db15
faiza203 <fiazfaiza981@gmail.com>
2020-10-29 13:15:42 +0500

    Import Line and Bar in Charts.jsx

--- src/components/Charts/Charts.jsx
+++ src/components/Charts/Charts.jsx
@@ -1,7 +1,11 @@
-import React, {useState , useEffect} from "react";
-import {fetchDailyData} from "../../api";
-console.log(fetchDailyData)
- const Charts = () => {
+import React , {useState , useEffect}from "react";
+import {Line , Bar} from "react-chartjs-2";
+import {fetchDailyData} from "./../../api";
+const Charts = () => {
+    const [dailyData , setDailyData] = useState({});
+    useEffect(async()=>{
+    setDailyData(await fetchDailyData()) 
+    })
     return <h1>Charts</h1>
 };
 
