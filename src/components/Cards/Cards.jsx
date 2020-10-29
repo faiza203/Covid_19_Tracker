17ea88e86402f3c58d6574123d56539d75c302f3
faiza203 <fiazfaiza981@gmail.com>
2020-10-29 12:18:59 +0500

    Import react-countup

--- src/components/Cards/Cards.jsx
+++ src/components/Cards/Cards.jsx
@@ -1,28 +1,30 @@
 import React from "react";
 import { Card, CardContent, Typography, Grid } from "@material-ui/core";
+import CountUp from "react-countup"
 const Cards = ({data : {confirmed ,recovered , deaths , lastUpdate}}) => {
     if(!confirmed){
         return "Loading"
     }
-    return <div className="contanier"><Grid contanier spacing={3} justify="center">
+    return <div className="contanier">
+    <Grid container={true} spacing={3} justify="center">
     <Grid item component={Card}>
     <CardContent>
     <Typography color="textSecondary" gutterBottom>Infected</Typography>
-    <Typography variant="45">{confirmed.value}</Typography>
+    <Typography variant="h5">{confirmed.value}</Typography>
     <Typography color="textSecondary" >Real Data</Typography>
     <Typography variant="body2">Number of active cases of COVID-19</Typography>
     </CardContent></Grid>
     <Grid item component={Card}>
     <CardContent>
     <Typography color="textSecondary" gutterBottom>Recovered</Typography>
-    <Typography variant="45">Real Data</Typography>
+    <Typography variant="h5">Real Data</Typography>
     <Typography color="textSecondary" >Real Data</Typography>
     <Typography variant="body2">Number of recovered cases of COVID-19</Typography>
     </CardContent></Grid>
     <Grid item component={Card}>
     <CardContent>
     <Typography color="textSecondary" gutterBottom>Deaths</Typography>
-    <Typography variant="45">Real Data</Typography>
+    <Typography variant="h5">Real Data</Typography>
     <Typography color="textSecondary" >Real Data</Typography>
     <Typography variant="body2">Number of deaths cases of COVID-19</Typography>
     </CardContent></Grid>
