7ce6cde77907eec98656f5305da7927fe433d117
faiza203 <fiazfaiza981@gmail.com>
2020-10-29 12:12:10 +0500

    Add COVID-19 confirmed cases

--- src/components/Cards/Cards.jsx
+++ src/components/Cards/Cards.jsx
@@ -1,6 +1,31 @@
 import React from "react";
 import { Card, CardContent, Typography, Grid } from "@material-ui/core";
-const Cards = (props) => {
-    return <div className="contanier">Cards</div>
+const Cards = ({data : {confirmed ,recovered , deaths , lastUpdate}}) => {
+    if(!confirmed){
+        return "Loading"
+    }
+    return <div className="contanier"><Grid contanier spacing={3} justify="center">
+    <Grid item component={Card}>
+    <CardContent>
+    <Typography color="textSecondary" gutterBottom>Infected</Typography>
+    <Typography variant="45">{confirmed.value}</Typography>
+    <Typography color="textSecondary" >Real Data</Typography>
+    <Typography variant="body2">Number of active cases of COVID-19</Typography>
+    </CardContent></Grid>
+    <Grid item component={Card}>
+    <CardContent>
+    <Typography color="textSecondary" gutterBottom>Recovered</Typography>
+    <Typography variant="45">Real Data</Typography>
+    <Typography color="textSecondary" >Real Data</Typography>
+    <Typography variant="body2">Number of recovered cases of COVID-19</Typography>
+    </CardContent></Grid>
+    <Grid item component={Card}>
+    <CardContent>
+    <Typography color="textSecondary" gutterBottom>Deaths</Typography>
+    <Typography variant="45">Real Data</Typography>
+    <Typography color="textSecondary" >Real Data</Typography>
+    <Typography variant="body2">Number of deaths cases of COVID-19</Typography>
+    </CardContent></Grid>
+    </Grid></div>
 }
 export default Cards;
